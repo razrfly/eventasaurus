03dc59dc98582d06c26348bd27ea70a37c3412ae
holden <holden@razrfly.com>
2025-05-07 11:49:09 +0200

    working show and index pages

--- lib/eventasaurus_web/controllers/dashboard_controller.ex
+++ lib/eventasaurus_web/controllers/dashboard_controller.ex
@@ -5,6 +5,17 @@ defmodule EventasaurusWeb.DashboardController do
   Display the user dashboard.
   """
   def index(conn, _params) do
-    render(conn, :index)
+    user = conn.assigns[:current_user]
+    events = if user, do: EventasaurusApp.Events.list_events_by_user(user), else: []
+    now = DateTime.utc_now()
+    upcoming_events =
+      events
+      |> Enum.filter(&(&1.start_at && DateTime.compare(&1.start_at, now) != :lt))
+      |> Enum.sort_by(& &1.start_at)
+    past_events =
+      events
+      |> Enum.filter(&(&1.start_at && DateTime.compare(&1.start_at, now) == :lt))
+      |> Enum.sort_by(& &1.start_at, :desc)
+    render(conn, :index, events: events, upcoming_events: upcoming_events, past_events: past_events)
   end
 end
