93541e18759b90ae0e376bd2b6eb0e02c518b8dd
holden <holden@razrfly.com>
2025-06-10 19:13:39 +0200

    dates fixing

--- lib/eventasaurus_web/controllers/event_html.ex
+++ lib/eventasaurus_web/controllers/event_html.ex
@@ -6,10 +6,41 @@ defmodule EventasaurusWeb.EventHTML do
   # No need for explicit render functions as Phoenix 1.7+ handles this automatically
   # when using embed_templates
 
-  # Helper function to format datetime
-  def format_datetime(%DateTime{} = dt) do
-    Calendar.strftime(dt, "%A, %B %d · %I:%M %p")
+  # Helper function to format datetime with timezone conversion
+  def format_datetime(%DateTime{} = dt, timezone \\ nil) do
+    converted_dt = if timezone do
+      EventasaurusWeb.TimezoneHelpers.convert_to_timezone(dt, timezone)
+    else
+      dt
+    end
+
+    Calendar.strftime(converted_dt, "%A, %B %d · %I:%M %p")
     |> String.replace(" 0", " ")
   end
-  def format_datetime(_), do: "Date not set"
+  def format_datetime(_, _), do: "Date not set"
+
+  # Helper function to format time only
+  def format_time(%DateTime{} = dt, timezone \\ nil) do
+    converted_dt = if timezone do
+      EventasaurusWeb.TimezoneHelpers.convert_to_timezone(dt, timezone)
+    else
+      dt
+    end
+
+    Calendar.strftime(converted_dt, "%I:%M %p")
+    |> String.replace(" 0", " ")
+  end
+  def format_time(_, _), do: ""
+
+  # Helper function to format date only
+  def format_date(%DateTime{} = dt, timezone \\ nil) do
+    converted_dt = if timezone do
+      EventasaurusWeb.TimezoneHelpers.convert_to_timezone(dt, timezone)
+    else
+      dt
+    end
+
+    Calendar.strftime(converted_dt, "%A, %B %d")
+  end
+  def format_date(_, _), do: ""
 end
