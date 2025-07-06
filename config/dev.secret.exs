38fe1940b46c6e8e03b58afdc1fce5342c355625
holden <holden@razrfly.com>
2025-07-06 16:33:37 +0200

    fixes for bugs

--- config/dev.secret.exs
+++ config/dev.secret.exs
@@ -13,3 +13,7 @@ config :eventasaurus, EventasaurusApp.Repo,
 
 # Stripe configuration has been moved to config/runtime.exs
 # This ensures it's loaded after .env files are processed
+
+# Disable PostHog for development to avoid console errors
+# To enable PostHog, set POSTHOG_PUBLIC_API_KEY in your .env file
+# System.put_env("POSTHOG_PUBLIC_API_KEY", "your_actual_api_key_here")
