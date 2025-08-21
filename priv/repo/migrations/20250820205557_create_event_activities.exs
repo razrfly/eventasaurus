a5d38d3984eda563e390fd25ba4593d697a927ef
holden <holden@razrfly.com>
2025-08-21 11:28:59 +0200

    code rabbit changes

--- priv/repo/migrations/20250820205557_create_event_activities.exs
+++ priv/repo/migrations/20250820205557_create_event_activities.exs
@@ -6,9 +6,9 @@ defmodule EventasaurusApp.Repo.Migrations.CreateEventActivities do
       add :event_id, references(:events, on_delete: :delete_all), null: false
       add :group_id, references(:groups, on_delete: :nilify_all)
       add :activity_type, :string, null: false
-      add :metadata, :jsonb, null: false, default: "{}"
+      add :metadata, :jsonb, null: false, default: fragment("'{}'::jsonb")
       add :occurred_at, :utc_datetime
-      add :created_by_id, references(:users, on_delete: :nilify_all), null: false
+      add :created_by_id, references(:users, on_delete: :nilify_all), null: true
       add :source, :string
       
       timestamps()
