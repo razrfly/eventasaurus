6be77fdadefb2b8d0030df67aab9f38909f9099a
holden <holden@razrfly.com>
2025-09-12 11:15:37 +0200

    fix js secruity

--- lib/eventasaurus_web/router.ex
+++ lib/eventasaurus_web/router.ex
@@ -31,9 +31,10 @@ defmodule EventasaurusWeb.Router do
       plug :put_root_layout, html: {EventasaurusWeb.Layouts, :root}
       plug :protect_from_forgery
       plug :put_secure_browser_headers
-      if Mix.env() != :dev do
-        plug EventasaurusWeb.Plugs.CSPPlug
-      end
+      # Temporarily disabled CSP while debugging
+      # if Mix.env() != :dev do
+      #   plug EventasaurusWeb.Plugs.CSPPlug
+      # end
       plug EventasaurusWeb.Plugs.SecurityPlug, force_https: true, security_headers: true
       plug :fetch_auth_user
       plug :assign_user_struct
@@ -56,9 +57,10 @@ defmodule EventasaurusWeb.Router do
     plug :put_root_layout, html: {EventasaurusWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
-    if Mix.env() != :dev do
-      plug EventasaurusWeb.Plugs.CSPPlug
-    end
+    # Temporarily disabled CSP while debugging
+    # if Mix.env() != :dev do
+    #   plug EventasaurusWeb.Plugs.CSPPlug
+    # end
     if Mix.env() == :dev do
       plug EventasaurusWeb.Dev.DevAuthPlug
     end
