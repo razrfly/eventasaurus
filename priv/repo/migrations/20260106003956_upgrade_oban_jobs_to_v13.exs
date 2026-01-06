8906c703174fbf5ce028f5be0b403346108e46b1
holden <holden@razrfly.com>
2026-01-05 18:46:00 -0600

    oban ups

---
+++ priv/repo/migrations/20260106003956_upgrade_oban_jobs_to_v13.exs
@@ -0,0 +1,16 @@
+defmodule EventasaurusApp.Repo.Migrations.UpgradeObanJobsToV13 do
+  @moduledoc """
+  Upgrade Oban schema to v13.
+
+  This migration adds compound indexes for cancelled_at and discarded_at columns
+  to significantly improve Oban.Plugins.Pruner performance when cleaning up
+  cancelled and discarded jobs.
+
+  Required for Oban 2.20+ for optimal Pruner performance.
+  See: https://hexdocs.pm/oban/changelog.html
+  """
+  use Ecto.Migration
+
+  def up, do: Oban.Migrations.up(version: 13)
+  def down, do: Oban.Migrations.down(version: 13)
+end
