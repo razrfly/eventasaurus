3838809cba0dcdd67b6b7293a26363a13aa58410
Holden Thomas <holden@razrfly.com>
2026-02-17 17:35:52 +0100

    one last

--- ios/Eventasaurus/Eventasaurus/EventasaurusApp.swift
+++ ios/Eventasaurus/Eventasaurus/EventasaurusApp.swift
@@ -3,8 +3,14 @@ import ClerkKit
 
 @main
 struct EventasaurusApp: App {
+    #if DEBUG
+    private static let clerkKey = "pk_test_cmFyZS1tdXNrb3gtODkuY2xlcmsuYWNjb3VudHMuZGV2JA"
+    #else
+    private static let clerkKey = "pk_live_Y2xlcmsud29tYmllLmNvbSQ"
+    #endif
+
     init() {
-        Clerk.configure(publishableKey: "pk_test_cmFyZS1tdXNrb3gtODkuY2xlcmsuYWNjb3VudHMuZGV2JA")
+        Clerk.configure(publishableKey: Self.clerkKey)
     }
 
     var body: some Scene {
